d650c5f2199e6fa17edda5d4020140e41a935741
Yurii <59282993+SHEV-4@users.noreply.github.com>
2025-04-29 16:40:09 +0300

    Add files via upload

---
+++ second_task.py
@@ -0,0 +1,14 @@
+import random
+
+def get_numbers_tickets(min,max,quantity = 1):
+    if min < 1:
+        return "Мінімальне значення менше ніж 1"
+    elif max > 1000:
+        return "Максимальне значення більше ніж 1000"
+    numbers_set = set()
+    for i in range(quantity):
+        numbers_set.add(random.randint(min,max))
+    
+    return numbers_set
+
+print(get_numbers_tickets(1,49,6))
