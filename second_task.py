e491db383320772af77643390528b83620f546ae
Yurii <59282993+SHEV-4@users.noreply.github.com>
2025-04-30 11:27:39 +0300

    Update second_task.py

--- second_task.py
+++ second_task.py
@@ -1,12 +1,21 @@
 import random
 
-def get_numbers_tickets(min,max,quantity = 1):
+def get_numbers_ticket(min,max,quantity = 1):
     if min < 1:
         return "Мінімальне значення менше ніж 1"
     elif max > 1000:
         return "Максимальне значення більше ніж 1000"
+    elif min > max:
+        return "Мінімалльне значення більше ніж максимальне значення"
+    elif max - min < quantity:
+        return "quantity більше ніж різниця між max і min"
+
     numbers_set = set()
-    for i in range(quantity):
+    while quantity > len(numbers_set):
         numbers_set.add(random.randint(min,max))
-    
-    return numbers_set
+        
+    numbers_list = []
+    for elem in numbers_set:
+        numbers_list.append(elem)
+        
+    return numbers_list
