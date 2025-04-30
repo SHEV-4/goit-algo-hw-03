c584863c2d0278d8107b684645c7e3cfb7e7c2c7
Yurii <59282993+SHEV-4@users.noreply.github.com>
2025-04-30 11:14:19 +0300

    Update first_task.py

--- first_task.py
+++ first_task.py
@@ -1,19 +1,10 @@
 from datetime import datetime
-import re
-def get_days_from_today(date):
-    without_separators_pattern = r"([;,\-:\.])"
-    without_separators_replacement = ""
-    without_separators_date = re.sub(without_separators_pattern,without_separators_replacement,date)
-    
-    pattern = r"(\d{4})(\d{2})(\d{2})"
-    replacement = r"\1-\3-\6"
-    formatted_date = re.sub(pattern,replacement,without_separators_date)
-    
-    now_date = datetime.now().date()
-    
-    current_date = datetime.strptime(formatted_date,"%Y-%m-%d").date()
-    
-    diff = now_date - current_date
-    
-    return diff.days
+def get_days_from_today(date:str):
+    try:
+        now_date = datetime.now()
+        current_date = datetime.strptime(date,"%Y-%m-%d")
+        diff = now_date - current_date
+        return diff.days
+    except ValueError:
+        return f'Дата має неправильний формат. Правильний формат "РРРР-ММ-ДД"'
     
