58705ede36f0d9050f5333790c07f9d788f92160
Yurii <59282993+SHEV-4@users.noreply.github.com>
2025-05-01 09:38:09 +0300

    Update second_task .py
    Повернення постого списку якщо даны некоректны

--- second_task.py
+++ second_task.py
@@ -1,20 +1,21 @@
 import random
 
 def get_numbers_ticket(min,max,quantity = 1):
+    numbers_list = []
     if min < 1:
-        return "Мінімальне значення менше ніж 1"
+        return numbers_list
     elif max > 1000:
-        return "Максимальне значення більше ніж 1000"
+        return numbers_list
     elif min > max:
-        return "Мінімалльне значення більше ніж максимальне значення"
+        return numbers_list
     elif max - min < quantity:
-        return "quantity більше ніж різниця між max і min"
+        return numbers_list
 
     numbers_set = set()
     while quantity > len(numbers_set):
         numbers_set.add(random.randint(min,max))
         
-    numbers_list = []
+    
     for elem in numbers_set:
         numbers_list.append(elem)
         
